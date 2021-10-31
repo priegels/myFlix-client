aed0a6aa6414a9ffaa8ce30872c94ec5f590baa4
priegels <88594441+priegels@users.noreply.github.com>
2021-10-31 13:00:12 +0100

    included axios library to fetch API from database

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+// axios library to import database's API
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -8,13 +11,22 @@ export class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-      ],
+      movies: [],
       selectedMovie: null
     };
+  }
+
+// adding ajax code to componentDidMount()  
+  componentDidMount(){
+    axios.get('http://k-flix.herokuapp.com/movies')
+    .then(response => {
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 
 //custom component method 
@@ -35,7 +47,7 @@ export class MainView extends React.Component {
         {selectedMovie 
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(newSelectedMovie) }}/>
           ))
         }
       </div>
