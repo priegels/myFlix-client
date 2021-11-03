6409236530bec65096ca1fdc355293bbfcb63d9b
priegels <88594441+priegels@users.noreply.github.com>
2021-11-03 12:23:41 +0100

    added login and registration view

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+export function RegistrationView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ Birthday, setBirthday ] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password, email, Birthday);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+     props.onRegistration(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Birthday:
+        <input type="date" value={Birthday} onChange={e => setBirthday(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
