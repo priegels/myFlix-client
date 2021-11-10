ccc398de5cd7ac0316565bca98619f727d17b3c7
priegels <88594441+priegels@users.noreply.github.com>
2021-11-10 11:32:10 +0100

    styling movie card view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import {Card, Button, Row, Col, Container} from 'react-bootstrap';
+
 import './movie-card.scss';
 import '../navbar/navbar.scss'
 
@@ -12,14 +14,20 @@ export class MovieCard extends React.Component {
     const { movie, onMovieClick } = this.props;
 
       return (
-        <Card backgroundColor={'Dark'} style={{ width: '16rem' }}>
-          <Card.Img className="MovieCard-Image" variant="top" src={movie.ImagePath} />
-          <Card.Body>
-            <Card.Title>{movie.Title}</Card.Title>
-            <Card.Text>{movie.Description}</Card.Text>
-            <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
-            </Card.Body>
-        </Card>
+        <Container fluid className="movie-card-container">
+          <Row>
+            <Col>
+            <Card className="movie-card" backgroundColor={'Dark'}>
+              <Card.Img className="movie-card-image" variant="top" src={movie.ImagePath} />
+              <Card.Body>
+                <Card.Title>{movie.Title}</Card.Title>
+                <Card.Text>{movie.Description}</Card.Text>
+                <Button className="movie-card-button" onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+              </Card.Body>
+            </Card>
+            </Col>
+          </Row>
+        </Container>
       );
     }
   }
