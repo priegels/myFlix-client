8244a214719eefb179a2c893f297cd287cabb25a
priegels <88594441+priegels@users.noreply.github.com>
2021-11-12 20:47:02 +0100

    specified fluid container

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container>
+      <Container fluid>
       <MainView />
       </Container>
     );
