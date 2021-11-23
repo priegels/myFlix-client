168328077464103f812469af994347d00d834893
priegels <88594441+priegels@users.noreply.github.com>
2021-11-23 19:20:47 +0100

    initialized new component

---
+++ src/components/movies-list/movies-list.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Container, Col } from 'react-bootstrap';
+import { connect } from 'react-redux';
+
+import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
+import { MovieCard } from '../movie-card/movie-card';
+
+import '../movies-list/movies-list.scss';
+
+const mapStateToProps = state => {
+  const { visibilityFilter } = state;
+  return { visibilityFilter };
+};
+
+function MoviesList(props) {
+  const { movies, visibilityFilter } = props;
+  let filteredMovies = movies;
+
+  if (visibilityFilter !== '') {
+    filteredMovies = movies.filter(m => m.Title.toLowerCase().includes(visibilityFilter.toLowerCase()));
+  }
+
+  if (!movies) return <div className="main-view"/>;
+
+  return <>
+  <Container fluid>
+    <Col className="filter-bar" md={12} style={{ margin: '1em' }}>
+      <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+    </Col>  
+  </Container>
+    {filteredMovies.map(m => (
+      <Col md={3} key={m._id}>
+        <MovieCard movie={m} />
+      </Col>
+    ))}
+  </>;
+}
+
+export default connect(mapStateToProps)(MoviesList);
