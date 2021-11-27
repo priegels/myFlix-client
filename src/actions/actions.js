af0f1b24dbdf86f5425c97d7cc3bd6dde78307c5
priegels <88594441+priegels@users.noreply.github.com>
2021-11-27 10:36:33 +0100

    added SET_USER and UPDATE_USER functions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,7 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_USER = 'SET_USER';
+export const UPDATE_USER = 'UPDATE USER';
 
 export function setMovies(value) {
   return { 
@@ -11,6 +13,20 @@ export function setMovies(value) {
 export function setFilter(value) {
   return { 
     type: SET_FILTER,
+    value
+  };
+}
+
+export function setUser(value) {
+  return {
+    type: SET_USER,
+    value
+  };
+}
+
+export function updateUser(value) {
+  return {
+    type: UPDATE_USER,
     value
   };
 }
