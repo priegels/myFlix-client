c1dc074b363ab6cfa173d4c98c33f23906accebc
priegels <88594441+priegels@users.noreply.github.com>
2021-12-02 14:38:22 +0100

    [fix] app correctly links to Genre/DirectorView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -30,6 +30,7 @@ export class MovieView extends React.Component {
   
   render() {
     const { movie, onBackClick } = this.props;
+    console.log('Genre', movie);
 
     return (
         <Container fluid>
@@ -41,40 +42,40 @@ export class MovieView extends React.Component {
             </Row>
             <Row className="movie-props">
               <Col>
-            <Row className="movie-description">
-              <Col>Description: </Col>
-            </Row>
-            <Row className="movie-description-text">
-              <Col className="value" md={12}>{movie.Description}</Col>
-            </Row>
-            <Row className="movie-release-date">
-              <Col>Release Date: </Col>
-            </Row>
-            <Row>
-              <Col className="movie-release-date value" md={12}>{movie.ReleaseDate}</Col>
-            </Row>
-            <Row className="movie-genre">
-              <Col className="label">Genre: </Col>
-              <Col className="value" md={12}>
-              <Link to={`/genres/${movie.Genre}`}>
-                <Button className="props-button" variant="link">{movie.Genre}</Button>
-              </Link>
-              </Col>
-            </Row>
+                <Row className="movie-description">
+                  <Col>Description: </Col>
+                </Row>
+                <Row className="movie-description-text">
+                  <Col className="value" md={12}>{movie.Description}</Col>
+                </Row>
+                <Row className="movie-release-date">
+                  <Col>Release Date: </Col>
+                </Row>
+                <Row>
+                  <Col className="movie-release-date value" md={12}>{movie.ReleaseDate}</Col>
+                </Row>
+                <Row className="movie-genre">
+                  <Col className="label">Genre: </Col>
+                  <Col className="value" md={12}>
+                  <Link to={`/genres/${movie.Genre.Name}`}>
+                    <Button className="props-button" variant="link">{movie.Genre.Name}</Button>
+                  </Link>
+                  </Col>
+                </Row>
 
-            <Row className="movie-director">
-              <Col className="label">Director: </Col>
-              <Col className="value" md={12}>
-              <Link to={`/directors/${movie.Director}`}>
-                <Button className="props-button" variant="link">Director</Button>
-              </Link>
+                <Row className="movie-director">
+                  <Col className="label">Director: </Col>
+                  <Col className="value" md={12}>
+                  <Link to={`/directors/${movie.Director.Name}`}>
+                    <Button className="props-button" variant="link">{movie.Director.Name}</Button>
+                  </Link>
+                  </Col>
+                </Row>
               </Col>
-            </Row>
-            </Col>
             </Row>
             <Row className="buttons">
-            <Button className="add-favorite-button" onClick={() => { this.onAddFavorite(movie._id) }}>Add Favorite</Button>
-            <Button className="movie-view-button" onClick={() => { onBackClick(null); }}>Back</Button>
+              <Button className="add-favorite-button" onClick={() => { this.onAddFavorite(movie._id) }}>Add Favorite</Button>
+              <Button className="movie-view-button" onClick={() => { onBackClick(null); }}>Back</Button>
             </Row>
         </Container>
        
